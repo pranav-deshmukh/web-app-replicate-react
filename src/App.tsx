b019fc8c082bc1f57fb2f97a51b4718032d375ec
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2025-03-16 23:03:41 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <img src="/FrontCover.webp" alt="logo" className="w-1/2" />
+    </div>
+  )
+}
+
+export default App
+
